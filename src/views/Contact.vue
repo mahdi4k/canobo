4803fb67be7d185e5ee7be372b350adf64cd0886
mahdi4k <falahati.mahdi@yahoo.com>
2022-07-18 09:14:57 +0430

    contact style

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,36 +1,54 @@
 <template>
   <div class="contact">
     <h2>Contacts</h2>
-    <p>
+    <p class="pt-contact">
       We are a research group in mostly biological backgrounds have interest in
       computer biology and bioinformatics. Our goal is to represents an online
       to which can everyone use complex and modern computational methods in
       biology and analyze Data more efficiently.
     </p>
 
-    <p>
+    <p class="pt-contact">
       Our team were get together in mid 2022 with 3 members. If you have
       interest in our work feel free to ask every aspects of them or at least
       emails us and say hi !
     </p>
-    <p>
-      The emails of the staffs were provided in Team section however the
+    <p class="pt-contact">
+      The emails of the staffs were provided in <span class="text-red">Team</span> section however the
       Institute have a its own Communications.
     </p>
 
     <div>
-      <div>
+      <div class="pt-contact">
         <span>Email</span>
-        <span>CONOBO@info.org </span>
+        <span class="text-red">CONOBO@info.org </span>
       </div>
-      <div>
+      <div class="pt-contact">
         <span>Youtube</span>
-        <span>CONOBO/YT.com </span>
+        <span class="text-red">CONOBO/YT.com </span>
       </div>
-      <div>
+      <div class="pt-contact">
         <span>Twitter</span>
-        <span>@CONOBO</span>
+        <span class="text-red">@CONOBO</span>
       </div>
     </div>
   </div>
 </template>
+<style lang="scss" scoped>
+.contact{
+    .pt-contact{
+        padding-top: 35px;
+    }
+    h2{
+        font-size: 40px;
+        color: #E52620;
+    }
+    p{
+        padding-top: 30px;
+        font-size: 35px;
+    }
+    .text-red{
+        color: #E52620;
+    }
+}
+</style>
